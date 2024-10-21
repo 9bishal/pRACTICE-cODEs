4098dbd101b6344b64f8ac7b4a3bc063e09c068d
Bishal kumar Shah <157871998+9bishal@users.noreply.github.com>
2024-10-21 22:58:04 +0530

    Add files via upload

---
+++ resizableRectangle.java
@@ -0,0 +1,38 @@
+interface resizable{
+    void resizewidht(int width);
+    void resizeheight(int height);
+}
+
+class rectangle implements resizable{
+    private int width;
+    private int height;
+
+    public rectangle(int width, int height){
+        this.width=width;
+        this.height=height;
+    }
+    //overide
+    public void resizewidht(int newWidth){
+        this.width=newWidth;
+        System.out.println("New Widht:" + this.width);
+    }
+
+    public void resizeheight(int newheight){
+        this.height=newheight;
+        System.out.println("New Heigth:" + this.height);
+    }
+
+    public void displaydimension(){ 
+    System.out.println("Current Dimension -> Width: " + width + "\nheight: " + height);    
+    }
+}
+
+public class resizableRectangle{
+    public static void main(String[] args){
+        rectangle react = new rectangle(8,9);
+        react.displaydimension();
+        react.resizeheight(9);
+        react.resizeheight(9);
+        react.resizeheight(9);
+    }
+}
