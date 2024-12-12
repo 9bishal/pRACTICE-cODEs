00397a05d100b0bc8d288052bc75def8cdd9d436
Bishal kumar Shah <shahvishal.9090@gmail.com>
2024-12-12 20:08:16 +0530

    Update Single_Level_Directory1.c

--- Single_Level_Directory1.c
+++ Single_Level_Directory1.c
@@ -1,29 +1,29 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     char directoryName[50], fileName[20][30];
     int fileSize[50];
     int numFiles;
     printf("Enter the name of the directory: ");
     scanf("%s", directoryName);
     printf("Enter the number of the files: ");
-    scanf("%d",&numFiles);
+    scanf("%d", &numFiles);
 
-    for(int i=0; i<numFiles; i++){
-        printf("Enter the name of %d file: ", i+1);
+    for (int i = 0; i < numFiles; i++)
+    {
+        printf("Enter the name of %d file: ", i + 1);
         scanf("%s", fileName[i]);
-        printf("Enter the size of %d file: ", i+1);
+        printf("Enter the size of %d file: ", i + 1);
         scanf("%d", &fileSize[i]);
     }
 
-        printf("Single Level Directory\n");
-        printf("Directory Name: %s\n", directoryName);
-        printf("Files\n");
-        for (int i = 0; i < numFiles; i++)
-        {
-            /* code */
-            printf("%d. %s (Size: %d in kb)\n", i+1, fileName[i], fileSize[i]);
-        }
-        return 0;
-
+    printf("Single Level Directory\n");
+    printf("Directory Name: %s\n", directoryName);
+    printf("Files\n");
+    for (int i = 0; i < numFiles; i++)
+    {
+        /* code */
+        printf("%d. %s (Size: %d in kb)\n", i + 1, fileName[i], fileSize[i]);
+    }
+    return 0;
 }
-
