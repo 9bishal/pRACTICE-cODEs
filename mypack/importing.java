f4c66dd1a44e791f08ee8f6343309f670a0d4f23
Bishal kumar Shah <shahvishal.9090@gmail.com>
2024-12-12 20:08:40 +0530

    practice code

---
+++ mypack/importing.java
@@ -0,0 +1,9 @@
+import mypack.new_package;
+
+public class importing {
+    public static void main(String[] args) {
+        new_package obj = new new_package();
+        obj.display();
+    }
+    
+}
